10431601ed8e65d86cdf9101999c26e93554d989
Kazik Pogoda <morisil@xemantic.com>
2021-12-09 16:59:17 +0100

    mail sender enabled

--- src/main/kotlin/DkjsSurveyProcessEngine.kt
+++ src/main/kotlin/DkjsSurveyProcessEngine.kt
@@ -1,10 +1,23 @@
+/*
+ * Copyright (c) 2021 Kazimierz Pogoda / Xemantic
+ */
+
 package de.dkjs.survey
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.mail.javamail.JavaMailSenderImpl
+import javax.inject.Singleton
 
 @SpringBootApplication
-open class DkjsSurveyProcessEngine
+open class DkjsSurveyProcessEngine {
+
+  @Singleton
+  @Bean
+  open fun mailSender() = JavaMailSenderImpl()
+
+}
 
 fun main(vararg args: String) {
 
