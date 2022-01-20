46f684ab8b46b6644336b41297ce108b30391b42
Kazik Pogoda <morisil@xemantic.com>
2022-01-20 20:59:46 +0100

    @DkjsSurveyProcessEngineTest extended: @AutoConfigureWebTestClient, AutoConfigureTestDatabase with cleaning db on each test

--- src/test/kotlin/test/DkjsSurveyProcessEngineTest.kt
+++ src/test/kotlin/test/DkjsSurveyProcessEngineTest.kt
@@ -2,26 +2,32 @@
  * Copyright (c) 2022 Kazimierz Pogoda / Xemantic
  */
 
-package de.dkjs.survey
+package de.dkjs.survey.test
 
 import io.mockk.mockk
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Import
 import org.springframework.mail.MailSender
+import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.ActiveProfiles
 
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Import(TestOverridesConfiguration::class)
 @ActiveProfiles("test", inheritProfiles = false)
+@AutoConfigureWebTestClient
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 annotation class DkjsSurveyProcessEngineTest
 
 @TestConfiguration
 open class TestOverridesConfiguration {
 
   @Bean
-  open fun mailSender(): MailSender = mockk<MailSender>()
+  open fun mailSender(): MailSender = mockk()
 
 }
