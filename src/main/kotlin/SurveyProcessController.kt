305d1b1dfdd5fbdedc44bf19ad9f23c9be793cf7
Kazik Pogoda <morisil@xemantic.com>
2022-01-26 18:50:06 +0100

    initial CSV error display UI

--- src/main/kotlin/SurveyProcessController.kt
+++ src/main/kotlin/SurveyProcessController.kt
@@ -4,41 +4,79 @@
 
 package de.dkjs.survey
 
+import de.dkjs.survey.model.Project
 import org.slf4j.Logger
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestPart
-import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
+import org.springframework.web.servlet.mvc.support.RedirectAttributes
 import javax.inject.Inject
+import javax.servlet.http.HttpSession
 
-
-@RestController
+@Controller
 class SurveyProcessController @Inject constructor(
   private val logger: Logger,
   private val parser: ProjectCsvParser,
   private val engine: DkjsSurveyProcessEngine
 ) {
 
+//  @GetMapping()
+//  fun index() = "index"
+
   // the controller does nothing at the moment, should support CSV file upload and parsing
 
   @PostMapping("/upload-projects")
   fun uploadProjects(
-    @RequestPart("projectsCsv") projectCsv: MultipartFile
+    @RequestPart("projectsCsv") projectCsv: MultipartFile,
+    attributes: RedirectAttributes,
+    session: HttpSession
   ): String {
 
-    try {
-      parser.parse(projectCsv)
-    } catch (e: CsvParsingException) {
-      logger.error("Error parsing CSV file: ${projectCsv.name}", e)
+    session.cleanDkjsAttributes()
+
+    if (projectCsv.isEmpty) {
+      attributes.addFlashAttribute(
+        "message",
+        "Please select a file to upload."
+      )
     }
 
-    // TODO render result in case of errors
+    try {
+      val projects = parser.parse(projectCsv)
+      session.setAttribute("projects", projects)
+    } catch (e: CsvParsingException) {
+      logger.error("Error parsing CSV file: ${projectCsv.name}", e)
+      attributes.addFlashAttribute("errors", e.rows)
+      session.setAttribute("hasErrors", true)
+    }
 
-//    redirectAttributes.addFlashAttribute(
-//      "message",
-//      "You successfully uploaded " + projectCsv.originalFilename + "!"
-//    )
     return "redirect:/"
+  }
+
+  @GetMapping("/submit-projects")
+  fun submitProjects(
+    session: HttpSession
+  ): String {
+    @Suppress("UNCHECKED_CAST")
+    val hasErrors = session.getAttribute("hasErrors") as Boolean?
+    if (hasErrors != null && hasErrors) {
+      logger.error("An attempt to submit projects with errors")
+      return "redirect:/"
+    }
+    @Suppress("UNCHECKED_CAST")
+    val projects: List<Project> = session.getAttribute("projects") as List<Project>
+    projects.forEach {
+      engine.handleNew(it)
+    }
+    session.cleanDkjsAttributes()
+    return "redirect:/"
+  }
+
+  private fun HttpSession.cleanDkjsAttributes() {
+    this.removeAttribute("projects")
+    this.removeAttribute("hasErrors")
   }
 
 }
