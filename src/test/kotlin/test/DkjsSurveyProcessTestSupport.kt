c76e166f942d21c6bf72bd675a67b52c600ad1c7
Kazik Pogoda <morisil@xemantic.com>
2022-03-14 18:21:25 +0100

    updated test support

--- src/test/kotlin/test/DkjsSurveyProcessTestSupport.kt
+++ src/test/kotlin/test/DkjsSurveyProcessTestSupport.kt
@@ -4,15 +4,20 @@
 
 package de.dkjs.survey.test
 
-import de.dkjs.survey.CsvParsingException
+import de.dkjs.survey.csv.CsvParsingException
 import org.slf4j.Logger
 import org.springframework.http.client.MultipartBodyBuilder
 import org.springframework.test.web.reactive.server.WebTestClient
 import org.springframework.web.reactive.function.BodyInserters
 import java.util.concurrent.TimeUnit
 import de.dkjs.survey.util.debug
+import io.kotest.assertions.assertSoftly
+import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
+import io.kotest.matchers.collections.shouldHaveAtLeastSize
 import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
+import java.time.LocalDate
+import java.time.LocalDateTime
 
 // utilities useful in testing
 
@@ -33,11 +38,24 @@ fun sleepForMaximalProcessDuration(logger: Logger, seconds: Int) {
   TimeUnit.SECONDS.sleep(seconds.toLong())
 }
 
-/**
- * Asserts that [CsvParsingException] has one and only one specified message.
- */
-infix fun CsvParsingException.shouldReport(message: String) {
-  this.rows shouldHaveSize 1
-  this.rows.first().messages shouldHaveSize 1
-  this.rows.first().messages.first() shouldBe message
+fun startOfDay(year: Int, month: Int, day: Int): LocalDateTime =
+  LocalDate.of(year, month, day).atStartOfDay()
+
+fun CsvParsingException.shouldReportRow(row: Int, vararg messages: String) {
+  require(row > 0) { "row numeration must start with 1" }
+  require(messages.isNotEmpty()) { "messages cannot be empty" }
+  if (messages.size == 1) {
+    rows[row - 1].messages[0] shouldBe messages[0]
+  } else {
+    assertSoftly {
+      rows shouldHaveAtLeastSize row
+      rows[row - 1].messages shouldContainExactlyInAnyOrder messages.toList()
+    }
+  }
+}
+
+infix fun CsvParsingException.shouldReportFirstRow(message: String) = assertSoftly {
+  rows shouldHaveSize 1
+  rows[0].messages shouldHaveSize 1
+  rows[0].messages[0] shouldBe message
 }
