3c5031559a10882c470c0932bab3dd05b50e5804
Kazik Pogoda <morisil@xemantic.com>
2022-04-13 19:53:44 +0200

    AlertEmailSender: proper annotations

--- src/main/kotlin/mail/AlertEmailSender.kt
+++ src/main/kotlin/mail/AlertEmailSender.kt
@@ -8,8 +8,12 @@ import org.slf4j.Logger
 import org.springframework.mail.MailException
 import org.springframework.mail.SimpleMailMessage
 import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.stereotype.Component
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
+@Component
 class AlertEmailSender @Inject constructor(
   private val logger: Logger,
   private val config: MailConfig,
