670e4309b3df55076d23517fd7964640776e7798
Kazik Pogoda <morisil@xemantic.com>
2022-04-14 19:22:12 +0200

    DkjsSurveyProcessEngineTest simplified

--- src/test/kotlin/test/DkjsSurveyProcessEngineTest.kt
+++ src/test/kotlin/test/DkjsSurveyProcessEngineTest.kt
@@ -4,25 +4,11 @@
 
 package de.dkjs.survey.test
 
-import io.mockk.mockk
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.context.TestConfiguration
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Import
-import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Import(TestOverridesConfiguration::class)
 @ActiveProfiles(profiles = ["test"], inheritProfiles = false)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 annotation class DkjsSurveyProcessEngineTest
-
-@TestConfiguration
-class TestOverridesConfiguration {
-
-  @Bean
-  fun mailSender(): JavaMailSender = mockk()
-
-}
