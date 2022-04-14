ed6ebc3eb5f526351751fa79a001a7272be0c816
Kazik Pogoda <morisil@xemantic.com>
2022-04-14 15:38:10 +0200

    process definitions

---
+++ src/main/kotlin/ScenarioPrePost.kt
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2022 Kazimierz Pogoda / Xemantic
+ */
+
+package de.dkjs.survey
+
+import de.dkjs.survey.engine.defineProcess
+import de.dkjs.survey.mail.MailType
+
+fun definePrePostProcess() = defineProcess {
+
+  sendImmediately(MailType.INFOMAIL_PRE_POST)
+
+  scheduleMailAt(time.oneWeekBeforeProjectStarts, MailType.REMINDER_1_T0)
+
+  scheduleIfNotSent(
+    time.oneWeekAfterProjectStarts,
+    setOf(
+      MailType.REMINDER_1_T1_RETRO,
+      MailType.REMINDER_2_T0,
+      MailType.INFOMAIL_T1
+    )
+  ) {
+    if (hasNoAnswers()) {
+      if (time.is14DaysProjectDuration) {
+        send(MailType.REMINDER_1_T1_RETRO) // master all german post
+      } else {
+        send(MailType.REMINDER_2_T0) // master all German PRE
+      }
+    } else {
+      scheduleAt(time.oneWeekBeforeProjectEnds) {
+        send(MailType.INFOMAIL_T1) // master all german post
+      }
+    }
+  }
+
+  if (!time.is14DaysProjectDuration) {
+    scheduleAt(time.twoWeeksAfterProjectStarts) {
+      sendAlert("message") // TODO what should be the message?
+    }
+  }
+
+  scheduleIfNotSent(
+    time.oneWeekAfterProjectEnds,
+    setOf(
+      MailType.REMINDER_2_T1_RETRO,
+      MailType.REMINDER_1_T1
+    )
+  ) {
+    if (hasNoAnswers()) {
+      if (time.is14DaysProjectDuration) {
+        send(MailType.REMINDER_2_T1_RETRO) // master all german post
+      } else {
+        send(MailType.REMINDER_1_T1) // master all german post
+      }
+    }
+  }
+
+  scheduleAt(time.twoWeeksAfterProjectEnds) {
+    if (hasNoAnswers()) {
+      sendAlert("message") // TODO what should be the message?
+    }
+    finishProcess()
+  }
+
+}
---
+++ src/main/kotlin/ScenarioRetro.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2022 Kazimierz Pogoda / Xemantic
+ */
+
+package de.dkjs.survey
+
+import de.dkjs.survey.engine.defineProcess
+import de.dkjs.survey.mail.MailType
+
+fun defineRetroProcess() = defineProcess {
+
+  sendImmediately(MailType.REMINDER_1_RETRO)
+
+  scheduleMailAt(time.oneWeekBeforeProjectEnds, MailType.REMINDER_1_RETRO)
+
+  scheduleMailAt(time.oneWeekAfterProjectEnds, MailType.REMINDER_2_RETRO)
+
+  scheduleAt(time.twoWeeksAfterProjectEnds) {
+    if (hasNoAnswers()) {
+      sendAlert("No answers received") // TODO what should be the message?
+    }
+    finishProcess()
+  }
+
+}
