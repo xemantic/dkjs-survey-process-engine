32fb7a7d96da2502a93e56f6dcf3b28889de3a6d
Kazik Pogoda <morisil@xemantic.com>
2022-04-14 12:50:23 +0200

    goals represented as List, not Set

--- src/test/kotlin/model/ProjectTest.kt
+++ src/test/kotlin/model/ProjectTest.kt
@@ -6,12 +6,11 @@ package de.dkjs.survey.model
 
 import io.kotest.matchers.collections.shouldContainExactly
 import io.kotest.matchers.collections.shouldHaveSize
-import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 import java.time.LocalDateTime
 import javax.validation.Validation
 
-class ModelValidationTest {
+class ProjectTest {
 
   @Test
   fun `should validate hierarchical project`() {
@@ -30,7 +29,7 @@ class ModelValidationTest {
         lastName  = "",
         email     = ""
       ),
-      goals       = emptySet(),
+      goals       = emptyList(),
       participants = Participants(
         age1to5   = -1,
         age6to10  = -1,
@@ -72,40 +71,6 @@ class ModelValidationTest {
         "provider.name - must not be empty",
         "status - must not be empty"
       )
-  }
-
-}
-
-class GoalsToAlphabetLettersTest {
-
-  @Test
-  fun `should convert goal to capital letter`() {
-    goalToSmallLetter(1) shouldBe 'a'
-    goalToSmallLetter(2) shouldBe 'b'
-    goalToSmallLetter(3) shouldBe 'c'
-    goalToSmallLetter(4) shouldBe 'd'
-    goalToSmallLetter(5) shouldBe 'e'
-    goalToSmallLetter(6) shouldBe 'f'
-    goalToSmallLetter(7) shouldBe 'g'
-  }
-
-  @Test
-  fun `should convert goal to small letter`() {
-    goalToCapitalLetter(1) shouldBe 'A'
-    goalToCapitalLetter(2) shouldBe 'B'
-    goalToCapitalLetter(3) shouldBe 'C'
-    goalToCapitalLetter(4) shouldBe 'D'
-    goalToCapitalLetter(5) shouldBe 'E'
-    goalToCapitalLetter(6) shouldBe 'F'
-    goalToCapitalLetter(7) shouldBe 'G'
-  }
-
-  @Test
-  fun `should return goals as alphabet letters`() {
-    goalsToCapitalLetters(setOf(1)) shouldBe ""
-    goalsToCapitalLetters(setOf(1, 2, 3)) shouldBe "B, C"
-    goalsToCapitalLetters(setOf(1, 5, 2)) shouldBe "B, E"
-    goalsToCapitalLetters(setOf(1, 5, 2, 3)) shouldBe "B, C, E"
   }
 
 }
