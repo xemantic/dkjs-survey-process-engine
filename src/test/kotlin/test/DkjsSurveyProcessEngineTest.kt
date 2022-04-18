eca387f0d270282768e6b900b29ed7ce65dd736e
Kazik Pogoda <morisil@xemantic.com>
2022-04-18 18:27:21 +0200

    active profiles removed from DkjsSurveyProcessEngineTest

--- src/test/kotlin/test/DkjsSurveyProcessEngineTest.kt
+++ src/test/kotlin/test/DkjsSurveyProcessEngineTest.kt
@@ -6,9 +6,7 @@ package de.dkjs.survey.test
 
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ActiveProfiles(profiles = ["test"], inheritProfiles = false)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 annotation class DkjsSurveyProcessEngineTest
